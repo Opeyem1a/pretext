63e2015b70f6e87a38d53ff4f65571617edde2b1
Opey Adeyemi <60050089+Opeyem1a@users.noreply.github.com>
2025-06-02 03:10:49 -0400

    output build into browser-specific dist directories (#11)
    * output build into browser-specific dist directories
    
    * lint

--- vite.defs.config.ts
+++ vite.defs.config.ts
@@ -10,18 +10,21 @@ interface StandaloneCssConfig {
     assetFileNames: string;
 }
 
+const BASE_DIST_DIR =
+    process.env.BROWSER === 'firefox' ? 'dist/firefox' : 'dist/chrome';
+
 const STANDALONE_SCRIPT_CONFIG_DEFS: Record<
     string,
     StandaloneIIFEScriptConfig
 > = {
     ui: {
         target: 'src/content/main/main.ts',
-        outDir: 'dist/content',
+        outDir: `${BASE_DIST_DIR}/content`,
         entryFileNames: 'main.js',
     },
     background: {
         target: 'src/background.ts',
-        outDir: 'dist/background',
+        outDir: `${BASE_DIST_DIR}/background`,
         entryFileNames: 'background.js',
     },
 };
@@ -29,10 +32,14 @@ const STANDALONE_SCRIPT_CONFIG_DEFS: Record<
 const STANDALONE_CSS_CONFIG_DEFS: Record<string, StandaloneCssConfig> = {
     main: {
         target: 'src/content/main/main.css',
-        outDir: 'dist/content',
+        outDir: `${BASE_DIST_DIR}/content`,
         assetFileNames: 'main.css',
     },
 };
 
 export type { StandaloneIIFEScriptConfig, StandaloneCssConfig };
-export { STANDALONE_SCRIPT_CONFIG_DEFS, STANDALONE_CSS_CONFIG_DEFS };
+export {
+    BASE_DIST_DIR,
+    STANDALONE_SCRIPT_CONFIG_DEFS,
+    STANDALONE_CSS_CONFIG_DEFS,
+};
